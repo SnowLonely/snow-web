881868f3ad67d1bf6471ab0e5f3555e6c62a7060
lihongzeng <lihongzeng@dmhxm.com>
2022-01-06 18:24:46 +0800

    添加一个配置

--- src/layouts/Menus.vue
+++ src/layouts/Menus.vue
@@ -5,6 +5,7 @@
       :open-keys="openKeys"
       mode="inline"
       theme="dark"
+      :inline-collapsed="collapsed"
       @openChange="onOpenChange"
       @click="onMenuClick"
     >
@@ -35,6 +36,7 @@ export default {
       defaultSelected: [],
       submitKeys: [],
       openKeys: [],
+      collapsed: false,
       menus: [
         {
           key: "1",
